dd90c50d61d02b8fa85297900607deafa9043c3a
lollcat <laurencemidgley@gmail.com>
2023-04-08 14:46:31 +0100

    feat: add step size adaption and batching to hmc + complete test

--- fabjax/mcmc/hmc.py
+++ fabjax/mcmc/hmc.py
@@ -1,9 +1,10 @@
-from typing import Tuple, Callable, NamedTuple
+from typing import Tuple, Callable, NamedTuple, Optional
 from functools import partial
 
 import chex
 import jax.numpy as jnp
 import jax.random
+from blackjax.adaptation.step_size import dual_averaging_adaptation
 
 
 from fabjax.base import TransitionOperator, Point, LogProbFn
@@ -13,6 +14,7 @@ from fabjax.mcmc.blackjax_hmc_rewrite import kernel as hmc_kernel, init as hmc_i
 class HMCState(NamedTuple):
     key: chex.PRNGKey
     inverse_mass_maxtric: chex.Array
+    adaption_state: Optional[chex.ArrayTree] = None
 
 
 def build_blackjax_hmc(
@@ -20,16 +22,22 @@ def build_blackjax_hmc(
                  n_outer_steps: int = 1,
                  n_inner_steps: int = 5,
                  init_step_size: float = 1e-4,
+                 adapt_step_size: bool = True,
                  alpha: float = 2.0,
+                 target_p_accept: float = 0.65
 ) -> TransitionOperator:
     # TODO: Use alpha as an argument.
 
     one_step = hmc_kernel(divergence_threshold=1000)
 
+    if adapt_step_size:
+        adapt_init, adapt_update, _ = dual_averaging_adaptation(target_p_accept, )
+
 
     def init(key: chex.PRNGKey) -> HMCState:
         inverse_mass_matrix = jnp.ones(dim)
-        return HMCState(key, inverse_mass_matrix)
+        adaption_state = adapt_init(init_step_size) if adapt_step_size else None
+        return HMCState(key, inverse_mass_matrix, adaption_state)
 
     def step(point: Point,
              transition_operator_state: HMCState,
@@ -39,23 +47,36 @@ def build_blackjax_hmc(
              ) -> \
             Tuple[Point, chex.ArrayTree, dict]:
 
-        hmc_state = hmc_init(point, beta)
+        chex.assert_rank(point.x, 2)
+        batch_size = point.x.shape[0]
+
+        hmc_state = jax.vmap(hmc_init, in_axes=(0, None))(point, beta)
 
         def scan_fn(body, xs):
             key = xs
-            hmc_state = body
-            hmc_state, info = one_step(
-                key,
-                hmc_state,
-                log_q_fn=log_q_fn,
-                log_p_fn=log_p_fn,
-                step_size=init_step_size,
-                inverse_mass_matrix=transition_operator_state.inverse_mass_maxtric,
-                num_integration_steps=n_inner_steps)
-            return hmc_state, info
+            key_batch = jax.random.split(key, batch_size)
+            hmc_state, transition_operator_state = body
+            step_size = jnp.exp(transition_operator_state.adaption_state.log_step_size) if adapt_step_size else \
+                init_step_size
+            step_fn_partial = partial(one_step,
+                                      log_q_fn=log_q_fn,
+                                        log_p_fn=log_p_fn,
+                                        step_size=step_size,
+                                        inverse_mass_matrix=transition_operator_state.inverse_mass_maxtric,
+                                        num_integration_steps=n_inner_steps)
+            hmc_state, info = jax.vmap(step_fn_partial)(key_batch, hmc_state)
+            if adapt_step_size:
+                new_adaption_state = adapt_update(transition_operator_state.adaption_state,
+                                                  jnp.mean(info.acceptance_rate))
+                transition_operator_state = transition_operator_state._replace(adaption_state=new_adaption_state)
+            return (hmc_state, transition_operator_state), info
+
 
         key, subkey = jax.random.split(transition_operator_state.key)
-        hmc_state, infos = jax.lax.scan(scan_fn, hmc_state, jax.random.split(subkey, n_outer_steps))
+        (hmc_state, transition_operator_state), infos = jax.lax.scan(
+            scan_fn,
+            (hmc_state, transition_operator_state),
+            jax.random.split(subkey, n_outer_steps))
 
         info = {f"mean_acceptance_rate": jnp.mean(infos.acceptance_rate)}
         point_kwargs = hmc_state._asdict()
--- fabjax/mcmc/hmc_test.py
+++ fabjax/mcmc/hmc_test.py
@@ -5,26 +5,7 @@ import matplotlib.pyplot as plt
 
 from fabjax.mcmc.hmc import build_blackjax_hmc
 from fabjax.base import Point, create_point
-from fabjax.utils.test import assert_trees_all_different
 from fabjax.utils.plot import plot_contours_2D, plot_marginal_pair
-
-
-def test_hmc_does_not_smoke():
-    hmc_transition_operator = build_blackjax_hmc(dim=2)
-
-    log_q_fn = lambda x: jnp.sum(x ** 2)
-    log_p_fn = lambda x: jnp.sum(x ** 3)
-
-    x = Point(jnp.ones((2,)), jnp.ones(()), jnp.ones(()), jnp.ones((2,)),
-              jnp.ones((2,)))
-
-    key = jax.random.PRNGKey(0)
-
-    hmc_state = hmc_transition_operator.init(key)
-
-    x_new, hmc_state, info = hmc_transition_operator.step(x, hmc_state, 1.0, log_q_fn, log_p_fn)
-
-    assert_trees_all_different(x, x_new)
 
 
 def test_hmc_produces_good_samples():
@@ -34,9 +15,10 @@ def test_hmc_produces_good_samples():
     key = jax.random.PRNGKey(0)
     batch_size = 1000
     n_outer_steps = 100
+    target_p_accept = 0.65
 
     hmc_transition_operator = build_blackjax_hmc(dim=2, n_outer_steps=n_outer_steps,
-                                                 init_step_size=1e-1)
+                                                 init_step_size=1e-4)
 
     loc_q = jnp.zeros((dim,))
     dist_q = distrax.MultivariateNormalDiag(loc_q, jnp.ones((dim,)))
@@ -49,12 +31,11 @@ def test_hmc_produces_good_samples():
     positions = dist_q.sample(seed=key, sample_shape=(batch_size,))
     points = jax.vmap(create_point, in_axes=(0, None, None))(positions, dist_q.log_prob, dist_p.log_prob)
 
-    key_batch = jax.random.split(key, batch_size)
-    hmc_state = jax.vmap(hmc_transition_operator.init)(key_batch)
+
+    hmc_state = hmc_transition_operator.init(key)
 
     # Run MCMC chain.
-    x_new, hmc_state, info = jax.vmap(hmc_transition_operator.step, in_axes=(0, 0, None, None, None))(
-        points, hmc_state, beta, dist_q.log_prob, dist_p.log_prob)
+    x_new, hmc_state, info = hmc_transition_operator.step(points, hmc_state, beta, dist_q.log_prob, dist_p.log_prob)
 
     # Visualise samples.
     bound = 10
@@ -73,6 +54,8 @@ def test_hmc_produces_good_samples():
     x_mean = jnp.mean(x_new.x, axis=0)
 
     assert ((x_mean - loc_p)**2 < (x_mean - loc_q)).all()
+
+    assert (0.6 < info['mean_acceptance_rate']) and (info['mean_acceptance_rate'] < 0.7)
 
 
 
