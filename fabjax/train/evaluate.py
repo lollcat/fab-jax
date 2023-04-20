3e287b5ccbfb8e6f21c282dea9f6538d186e1571
Laurence Midgley <laurencemidgley@gmail.com>
2023-04-20 16:43:56 +0100

    feat: add evaluation (#13)

---
+++ fabjax/train/evaluate.py
@@ -0,0 +1,48 @@
+from typing import Union, Tuple, Callable
+
+import chex
+import jax
+import jax.numpy as jnp
+from fabjax.sampling.smc import SequentialMonteCarloSampler
+from fabjax.sampling.resampling import log_effective_sample_size
+from fabjax.flow.flow import Flow, FlowParams
+from fabjax.train.fab_with_buffer import TrainStateWithBuffer
+from fabjax.train.fab_without_buffer import TrainStateNoBuffer
+
+
+def setup_fab_eval_function(
+                      flow: Flow,
+                      ais: SequentialMonteCarloSampler,
+                      log_p_x,
+                      batch_size: int,
+                      inner_batch_size: int
+) -> Callable[[chex.ArrayTree, chex.PRNGKey], dict]:
+    """Evaluate the ESS of the flow, and AIS. """
+    assert ais.alpha == 1.  # Make sure target is set to p.
+    assert ais.use_resampling is False  # Make sure we are doing AIS, not SMC.
+
+    
+    @jax.jit
+    def eval_fn(state: Union[TrainStateNoBuffer, TrainStateWithBuffer], key: chex.PRNGKey):
+        def log_q_fn(x: chex.Array) -> chex.Array:
+            return flow.log_prob_apply(state.flow_params, x)
+        
+        def inner_fn(carry: None, xs: chex.PRNGKey) -> Tuple[None, Tuple[chex.Array, chex.Array]]:
+            """Perform SMC forward pass and grab just the importance weights."""
+            key = xs
+            x0, log_q_flow = flow.sample_and_log_prob_apply(state.flow_params, key, (batch_size,))
+            point, log_w, smc_state, smc_info = ais.step(x0, state.smc_state, log_q_fn, log_p_x)
+            log_w_flow = log_p_x(x0) - log_q_flow
+            return None, (log_w_flow, log_w)
+        
+        # Run scan function.
+        n_batches = (batch_size // inner_batch_size) + 1
+        _, (log_w_flow, log_w_ais) = jax.lax.scan(inner_fn, init=None, xs=jax.random.split(key, n_batches))
+    
+        # Compute metrics
+        info = {}
+        info.update(eval_ess_flow=jnp.exp(log_effective_sample_size(log_w_flow.flatten())),
+                    eval_ess_ais=jnp.exp(log_effective_sample_size(log_w_ais.flatten())))
+        return info
+
+    return eval_fn
