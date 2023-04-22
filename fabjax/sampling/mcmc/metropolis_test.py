fix: bug in metropolis accept calculation (#14)

fabjax/sampling/mcmc/metropolis_test.py
@@ -11,27 +11,29 @@ from fabjax.utils.plot import plot_contours_2D, plot_marginal_pair, plot_history
 from fabjax.utils.logging import ListLogger
 
 
-def test_metropolis_produces_good_samples():
+def tesst_metropolis_produces_good_samples():
 
     dim = 2
-    beta = 1.  # Equivalent to setting target to p.
+    beta = 0.5  # Equivalent to setting target to p.
     key = jax.random.PRNGKey(0)
     batch_size = 1000
     n_outer_steps = 100
     alpha = 2.
 
-    metropolis_trans_op = build_metropolis(dim=2, n_steps=n_outer_steps, init_step_size=1., tune_step_size=True)
+    metropolis_trans_op = build_metropolis(dim=2,
+                                           n_steps=n_outer_steps,
+                                           init_step_size=1.,
+                                           tune_step_size=True,
+                                           step_size_multiplier=1.1)
 
     loc_q = jnp.zeros((dim,))
     dist_q = distrax.MultivariateNormalDiag(loc_q, jnp.ones((dim,)))
 
-    loc_p = jnp.zeros((dim,)) + 3
-    scale_p = jnp.ones((dim,)) * 0.5
+    loc_p = jnp.zeros((dim,)) + 1
+    scale_p = jnp.ones((dim,))
     dist_p = distrax.MultivariateNormalDiag(loc_p, scale_p)
 
-    # Initialise MCMC chain.
-    positions = dist_q.sample(seed=key, sample_shape=(batch_size,))
-    points = jax.vmap(create_point, in_axes=(0, None, None, None))(positions, dist_q.log_prob, dist_p.log_prob, False)
+
 
 
     trans_op_state = metropolis_trans_op.init(key)
@@ -40,8 +42,18 @@ def test_metropolis_produces_good_samples():
 
     # Run MCMC chain.
     for i in range(50):
-        x_new, trans_op_state, info = metropolis_trans_op.step(points, trans_op_state, beta, alpha,
-                                                              dist_q.log_prob, dist_p.log_prob)
+        # Initialise MCMC chain.
+        key, subkey = jax.random.split(key)
+        positions = dist_q.sample(seed=subkey, sample_shape=(batch_size,))
+        points = jax.vmap(create_point, in_axes=(0, None, None, None))(positions, dist_q.log_prob, dist_p.log_prob,
+                                                                       False)
+        x_new, trans_op_state, info = metropolis_trans_op.step(
+                                                              point=points,
+                                                              transition_operator_state=trans_op_state,
+                                                              beta=beta,
+                                                              alpha=alpha,
+                                                              log_q_fn=dist_q.log_prob,
+                                                              log_p_fn=dist_p.log_prob)
         logger.write(info)
 
     plot_history(logger.history)
@@ -49,13 +61,17 @@ def test_metropolis_produces_good_samples():
 
     # Visualise samples.
     bound = 10
-    fig, axs = plt.subplots(1, 2)
+    fig, axs = plt.subplots(1, 3, sharex=True, sharey=True, figsize=(12, 4))
     plot_contours_2D(dist_q.log_prob, axs[0], bound=bound)
     plot_marginal_pair(x_new.x, axs[0], bounds=(-bound, bound), alpha=0.2)
     plot_contours_2D(dist_p.log_prob, axs[1], bound=bound)
     plot_marginal_pair(x_new.x, axs[1], bounds=(-bound, bound), alpha=0.2)
+    plot_contours_2D(dist_p.log_prob, axs[2], bound=bound)
+    plot_marginal_pair(dist_p.sample(seed=key, sample_shape=(batch_size,)),
+                       axs[2], bounds=(-bound, bound), alpha=0.2)
     axs[0].set_title("samples vs log prob q contours")
     axs[1].set_title("samples vs log prob p contours")
+    axs[2].set_title("p samples vs p contours")
     plt.tight_layout()
     plt.show()
 
@@ -68,4 +84,5 @@ def test_metropolis_produces_good_samples():
     print(info)
 
 
-
+if __name__ == '__main__':
+    tesst_metropolis_produces_good_samples()

fabjax/sampling/smc.py
@@ -73,16 +73,19 @@ def ais_inner_transition(point: Point, log_w: chex.Array, trans_op_state: chex.A
     """Perform inner iteration of AIS, incrementing the log_w appropriately."""
     beta = betas[ais_step_index]
 
-    new_point, trans_op_state, info = transition_operator.step(point, trans_op_state, beta, alpha, log_q_fn, log_p_fn)
+    new_point, trans_op_state, info = transition_operator.step(
+        point=point, transition_operator_state=trans_op_state,
+        beta=beta, alpha=alpha, log_q_fn=log_q_fn, log_p_fn=log_p_fn)
 
     # Remove invalid samples.
     valid_samples = jnp.isfinite(new_point.log_q) & jnp.isfinite(new_point.log_p) & \
                     jnp.alltrue(jnp.isfinite(new_point.x), axis=-1)
-    point = jax.tree_map(lambda a, b: broadcasted_where(valid_samples, a, b), new_point, point)
+    info.update(n_valid_samples = jnp.sum(valid_samples))
+    new_point = jax.tree_map(lambda a, b: broadcasted_where(valid_samples, a, b), new_point, point)
 
-    log_w = log_w + log_weight_contribution_point(point, ais_step_index, betas, alpha)
+    log_w = log_w + log_weight_contribution_point(new_point, ais_step_index, betas, alpha)
 
-    return (point, log_w), (trans_op_state, info)
+    return (new_point, log_w), (trans_op_state, info)
 
 
 def replace_invalid_samples_with_valid_ones(point: Point, key: chex.PRNGKey) -> Point:
@@ -103,7 +106,7 @@ def build_smc(
         n_intermediate_distributions: int,
         spacing_type: str = 'linear',
         alpha: float = 2.,
-        use_resampling: bool = True,
+        use_resampling: bool = False,
         resampling_threshold: float = 0.3
               ) -> SequentialMonteCarloSampler:
     """
@@ -176,7 +179,7 @@ def build_smc(
 
         # Sometimes the flow produces nan samples - remove these.
         key, subkey = jax.random.split(smc_state.key)
-        point0 = replace_invalid_samples_with_valid_ones(point0, key)
+        point0 = replace_invalid_samples_with_valid_ones(point0, subkey)
 
         log_w_init = log_weight_contribution_point(point0, 0, betas=betas, alpha=alpha)
         log_w = log_w_init
@@ -192,10 +195,11 @@ def build_smc(
                 point, log_w, log_ess = optionally_resample(key=key, log_weights=log_w, samples=point,
                                                    resample_threshold=resampling_threshold)
                 info.update(ess=jnp.exp(log_ess))
-            (point, log_w), (trans_op_state, info_transition) = ais_inner_transition(point, log_w, trans_op_state, betas,
-                                                                          ais_step_index,
-                                                                          transition_operator, log_q_fn, log_p_fn,
-                                                                          alpha)
+            (point, log_w), (trans_op_state, info_transition) = ais_inner_transition(
+                point, log_w, trans_op_state, betas,
+                ais_step_index,
+                transition_operator, log_q_fn, log_p_fn,
+                alpha)
             info.update(info_transition)
             return (point, log_w), (trans_op_state, info)
 