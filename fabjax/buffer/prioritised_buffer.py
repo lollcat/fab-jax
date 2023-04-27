feat: fix buffer sampling and stability, option for additional reverse KL loss (makes things worse!) (#17)

fabjax/buffer/prioritised_buffer.py
@@ -9,9 +9,11 @@ from fabjax.utils.jax_util import broadcasted_where
 
 def sample_without_replacement(key: chex.Array, logits: chex.Array, n: int) -> chex.Array:
     # https://timvieira.github.io/blog/post/2014/07/31/gumbel-max-trick/
-    z = jax.random.gumbel(key=key, shape=logits.shape)
+    key1, key2 = jax.random.split(key)
+    z = jax.random.gumbel(key=key1, shape=logits.shape)
     # vals, indices = jax.lax.approx_max_k(z + logits, n)
     vals, indices = jax.lax.top_k(z + logits, n)
+    indices = jax.random.permutation(key2, indices)
     return indices
 
 
@@ -212,7 +214,7 @@ def build_prioritised_buffer(
         log_w, log_q = jax.tree_map(
             partial(broadcasted_where, valid_adjustment),
             (log_w, log_q),
-            (buffer_state.data.log_w[indices], buffer_state.data.log_q_old[indices]))
+            (-jnp.ones_like(log_w)*jnp.inf, jnp.zeros_like(log_q)))
 
         # adjust log weights in buffer state
         log_w = buffer_state.data.log_w.at[indices].set(log_w)

fabjax/train/fab_with_buffer.py
@@ -1,4 +1,4 @@
-from typing import Callable, NamedTuple, Tuple
+from typing import Callable, NamedTuple, Tuple, Optional
 
 import chex
 import jax.numpy as jnp
@@ -17,7 +17,7 @@ ParameterizedLogProbFn = Callable[[chex.ArrayTree, chex.Array], chex.Array]
 Info = dict
 
 
-def fab_loss_buffer_samples(
+def fab_loss_buffer_samples_fn(
         params: FlowParams,
         x: chex.Array,
         log_q_old: chex.Array,
@@ -34,6 +34,44 @@ def fab_loss_buffer_samples(
     chex.assert_equal_shape((log_q, log_w_adjust))
     w_adjust = jnp.clip(jnp.exp(log_w_adjust), a_max=w_adjust_clip)
     return - jnp.mean(w_adjust * log_q), (log_w_adjust, log_q)
+
+
+def reverse_kl_loss_fn(params: FlowParams,
+                       key: chex.PRNGKey,
+                       batch_size: int,
+                       sample_and_log_prob_apply: Callable,
+                       target_log_prob: LogProbFn,
+                       ) -> chex.Array:
+    """KL(q||p)."""
+    x, log_q = sample_and_log_prob_apply(params, key, (batch_size,))
+    log_p = target_log_prob(x)
+    kl = jnp.mean(log_q - log_p)
+    return kl
+
+
+def generic_loss(params: FlowParams,
+        x: chex.Array,
+        key: chex.PRNGKey,
+        log_q_old: chex.Array,
+        alpha: chex.Array,
+        flow: Flow,
+        w_adjust_clip: float,
+        use_reverse_kl_loss: bool = False,
+        target_log_prob: Optional[LogProbFn] = None
+        ) -> Tuple[chex.Array, Tuple[chex.Array, chex.Array, dict]]:
+    """Generic loss function for training.
+    Allows for experimentation for additions to the vanilla FAB loss (e.g. adding a reverse kl loss)."""
+    info = {}
+    fab_loss, (log_w_adjust, log_q) = fab_loss_buffer_samples_fn(params, x, log_q_old, alpha, flow.log_prob_apply,
+                                                                 w_adjust_clip)
+    info.update(fab_loss=fab_loss)
+    if use_reverse_kl_loss:
+        assert target_log_prob is not None
+        batch_size = x.shape[0]
+        rkld_loss = reverse_kl_loss_fn(params, key, batch_size, flow.sample_and_log_prob_apply, target_log_prob)
+        fab_loss = fab_loss + rkld_loss
+        info.update(rkld_loss=rkld_loss, total_loss=fab_loss)
+    return fab_loss, (log_w_adjust, log_q, info)
 
 
 class TrainStateWithBuffer(NamedTuple):
@@ -54,6 +92,7 @@ def build_fab_with_buffer_init_step_fns(
         n_updates_per_smc_forward_pass: int,
         alpha: float = 2.,
         w_adjust_clip: float = 10.,
+        use_reverse_kl_loss: bool = False
 ):
     """Create the `init` and `step` functions that define the FAB algorithm."""
     assert smc.alpha == alpha
@@ -72,7 +111,7 @@ def build_fab_with_buffer_init_step_fns(
             return flow.log_prob_apply(flow_params, x)
 
         def body_fn(carry, xs):
-            """fer."""
+            """Generate samples with AIS/SMC."""
             smc_state = carry
             key = xs
             x0 = flow.sample_apply(flow_params, key, (batch_size,))
@@ -91,23 +130,24 @@ def build_fab_with_buffer_init_step_fns(
         return TrainStateWithBuffer(flow_params=flow_params, key=key3, opt_state=opt_state,
                                     smc_state=smc_state, buffer_state=buffer_state)
 
-    def one_gradient_update(carry: Tuple[FlowParams, optax.OptState], xs: Tuple[chex.Array, chex.Array]):
+    def one_gradient_update(carry: Tuple[FlowParams, optax.OptState], xs: Tuple[chex.Array, chex.Array,
+    chex.PRNGKey]):
         """Perform on update to the flow parameters with a batch of data from the buffer."""
         flow_params, opt_state = carry
-        x, log_q_old = xs
+        x, log_q_old, key = xs
         info = {}
 
         # Estimate loss and update flow params.
-        (loss, (log_w_adjust, log_q)), grad = jax.value_and_grad(fab_loss_buffer_samples, has_aux=True)(
-            flow_params, x, log_q_old, alpha, flow.log_prob_apply, w_adjust_clip)
+        grad, (log_w_adjust, log_q, info) = jax.grad(generic_loss, has_aux=True)(
+            flow_params, x, key, log_q_old, alpha, flow, w_adjust_clip, use_reverse_kl_loss, log_p_fn)
         updates, new_opt_state = optimizer.update(grad, opt_state, params=flow_params)
         new_params = optax.apply_updates(flow_params, updates)
         grad_norm = optax.global_norm(grad)
-        info.update(loss=loss)
         info.update(log10_grad_norm=jnp.log10(grad_norm))  # Makes scale nice for plotting
         info.update(log10_max_param_grad=jnp.log(jnp.max(ravel_pytree(grad)[0])))
         if isinstance(opt_state, IgnoreNanOptState):
-            info.update(ignored_grad_count=opt_state.ignored_grads_count)
+            info.update(ignored_grad_count=opt_state.ignored_grads_count,
+                        total_optimizer_steps=opt_state.total_steps)
         return (new_params, new_opt_state), (info, log_w_adjust, log_q)
 
 
@@ -122,8 +162,10 @@ def build_fab_with_buffer_init_step_fns(
         x_buffer, log_q_old_buffer, indices = buffer.sample_n_batches(subkey, state.buffer_state, batch_size,
                                                                       n_updates_per_smc_forward_pass)
         # Perform sgd steps on flow.
+        key, subkey = jax.random.split(key)
         (new_flow_params, new_opt_state), (infos, log_w_adjust, log_q_old) = jax.lax.scan(
-            one_gradient_update, init=(state.flow_params, state.opt_state), xs=(x_buffer, log_q_old_buffer),
+            one_gradient_update, init=(state.flow_params, state.opt_state),
+            xs=(x_buffer, log_q_old_buffer, jax.random.split(subkey, n_updates_per_smc_forward_pass)),
             length=n_updates_per_smc_forward_pass
         )
         # Adjust samples in the buffer.