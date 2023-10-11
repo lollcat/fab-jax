06c746905bb774288670c5e71fe7743d9dbfded3
lollcat <laurencemidgley@gmail.com>
2023-10-11 16:30:22 +0100

    feat: switch to CRAFTs cox process code

--- fabjax/targets/cox.py
+++ fabjax/targets/cox.py
@@ -1,3 +1,5 @@
+"""Taken from https://github.com/google-deepmind/annealed_flow_transport/blob/master/annealed_flow_transport/densities.py."""
+
 from typing import List
 
 import os.path as osp
@@ -5,44 +7,161 @@ import pathlib
 
 import chex
 import jax.numpy as jnp
+import numpy as np
 import matplotlib.pyplot as plt
+import jax
 
 from fabjax.targets.base import Target
-from fabjax.targets.cox_utils import Cox
+from fabjax.targets import cox_utils as cp_utils
 
 
+Array = chex.Array
 
-class CoxDist(Target):
-    def __init__(self, num_bins_per_dim: int = 40):
-        fcsv = osp.join(pathlib.Path(__file__).parent.resolve(), "df_pines.csv")
-        self.num_bins_per_dim = num_bins_per_dim
-        self.cox = Cox(fcsv, num_bins_per_dim, use_whitened=False)
 
-        dim = int(num_bins_per_dim ** 2)
+class LogGaussianCoxPines(Target):
+  """Log Gaussian Cox process posterior in 2D for pine saplings data.
 
-        super().__init__(dim=dim, log_Z=self.gt_logz(), can_sample=False, n_plots=0,
-                         n_model_samples_eval=1000, n_target_samples_eval=2000)
+  This follows Heng et al 2020 https://arxiv.org/abs/1708.08396 .
 
-    def gt_logz(self):
-        if self.num_bins_per_dim == 40:
-            return 501.806  # from long run SMC
-        elif self.num_bins_per_dim == 32:
-            return 503.3939
-        else:
-            return None
+  config.file_path should point to a csv file of num_points columns
+  and 2 rows containg the Finnish pines data.
 
-    def log_prob(self, x: chex.Array):
-        batched = x.ndim == 2
-        if not batched:
-            x = x[None, ]
-        log_prob = self.cox.evaluate_log_density(x)
-        if not batched:
-            log_prob = jnp.squeeze(log_prob, axis=0)
-        return log_prob
+  config.use_whitened is a boolean specifying whether or not to use a
+  reparameterization in terms of the Cholesky decomposition of the prior.
+  See Section G.4 of https://arxiv.org/abs/2102.07501 for more detail.
+  The experiments in the paper have this set to False.
 
-    def visualise(self,
+  num_dim should be the square of the lattice sites per dimension.
+  So for a 40 x 40 grid num_dim should be 1600.
+  """
+
+  def __init__(self,
+               num_dim: int = 32*32,
+               use_whitened: bool = False):
+
+    # Discretization is as in Controlled Sequential Monte Carlo
+    # by Heng et al 2017 https://arxiv.org/abs/1708.08396
+    self._num_latents = num_dim
+    self._num_grid_per_dim = int(np.sqrt(num_dim))
+
+    file_path = osp.join(pathlib.Path(__file__).parent.resolve(), "df_pines.csv")
+    pines_array = self.get_pines_points(file_path)[1:, 1:]
+    bin_counts = jnp.array(
+        cp_utils.get_bin_counts(pines_array,
+                                self._num_grid_per_dim))
+
+    self._flat_bin_counts = jnp.reshape(bin_counts, (self._num_latents))
+
+    # This normalizes by the number of elements in the grid
+    self._poisson_a = 1./self._num_latents
+    # Parameters for LGCP are as estimated in Moller et al, 1998
+    # "Log Gaussian Cox processes" and are also used in Heng et al.
+
+    self._signal_variance = 1.91
+    self._beta = 1./33
+
+    self._bin_vals = cp_utils.get_bin_vals(self._num_grid_per_dim)
+
+    def short_kernel_func(x, y):
+      return cp_utils.kernel_func(x, y, self._signal_variance,
+                                  self._num_grid_per_dim, self._beta)
+
+    self._gram_matrix = cp_utils.gram(short_kernel_func, self._bin_vals)
+    self._cholesky_gram = jnp.linalg.cholesky(self._gram_matrix)
+    self._white_gaussian_log_normalizer = -0.5 * self._num_latents * jnp.log(
+        2. * jnp.pi)
+
+    half_log_det_gram = jnp.sum(jnp.log(jnp.abs(jnp.diag(self._cholesky_gram))))
+    self._unwhitened_gaussian_log_normalizer = -0.5 * self._num_latents * jnp.log(
+        2. * jnp.pi) - half_log_det_gram
+    # The mean function is a constant with value mu_zero.
+    self._mu_zero = jnp.log(126.) - 0.5*self._signal_variance
+
+    if use_whitened:
+      self._posterior_log_density = self.whitened_posterior_log_density
+    else:
+      self._posterior_log_density = self.unwhitened_posterior_log_density
+
+    super().__init__(dim=num_dim, log_Z=self.gt_logz(), can_sample=False, n_plots=0,
+                   n_model_samples_eval=1000, n_target_samples_eval=2000)
+
+  def get_pines_points(self, file_path):
+    """Get the pines data points."""
+    with open(file_path, "rt") as input_file:
+      b = np.genfromtxt(input_file, delimiter=",")
+    return b
+
+  def whitened_posterior_log_density(self, white: Array) -> Array:
+    quadratic_term = -0.5 * jnp.sum(white**2)
+    prior_log_density = self._white_gaussian_log_normalizer + quadratic_term
+    latent_function = cp_utils.get_latents_from_white(white, self._mu_zero,
+                                                      self._cholesky_gram)
+    log_likelihood = cp_utils.poisson_process_log_likelihood(
+        latent_function, self._poisson_a, self._flat_bin_counts)
+    return prior_log_density + log_likelihood
+
+  def unwhitened_posterior_log_density(self, latents: Array) -> Array:
+    white = cp_utils.get_white_from_latents(latents, self._mu_zero,
+                                            self._cholesky_gram)
+    prior_log_density = -0.5 * jnp.sum(
+        white * white) + self._unwhitened_gaussian_log_normalizer
+    log_likelihood = cp_utils.poisson_process_log_likelihood(
+        latents, self._poisson_a, self._flat_bin_counts)
+    return prior_log_density + log_likelihood
+
+  def log_prob(self, x: Array) -> Array:
+    chex.assert_rank(x, 1)
+    return self._posterior_log_density(x)
+
+  def gt_logz(self):
+    if self._num_grid_per_dim == 40:
+        return 501.806  # from long run SMC
+    elif self._num_grid_per_dim == 32:
+        return 503.3939
+    else:
+        return None
+
+  def visualise(self,
                   samples: chex.Array,
                   axes: List[plt.Axes],
                   ) -> None:
-        return None
+    return None
 
+CoxDist = LogGaussianCoxPines
+
+
+# class CoxDist(Target):
+# Using prev code
+#     def __init__(self, num_bins_per_dim: int = 40):
+#         fcsv = osp.join(pathlib.Path(__file__).parent.resolve(), "df_pines.csv")
+#         self.num_bins_per_dim = num_bins_per_dim
+#         self.cox = Cox(fcsv, num_bins_per_dim, use_whitened=False)
+#
+#         dim = int(num_bins_per_dim ** 2)
+#
+#         super().__init__(dim=dim, log_Z=self.gt_logz(), can_sample=False, n_plots=0,
+#                          n_model_samples_eval=1000, n_target_samples_eval=2000)
+#
+#     def gt_logz(self):
+#         if self.num_bins_per_dim == 40:
+#             return 501.806  # from long run SMC
+#         elif self.num_bins_per_dim == 32:
+#             return 503.3939
+#         else:
+#             return None
+#
+#     def log_prob(self, x: chex.Array):
+#         batched = x.ndim == 2
+#         if not batched:
+#             x = x[None, ]
+#         log_prob = self.cox.evaluate_log_density(x)
+#         if not batched:
+#             log_prob = jnp.squeeze(log_prob, axis=0)
+#         return log_prob
+#
+#     def visualise(self,
+#                   samples: chex.Array,
+#                   axes: List[plt.Axes],
+#                   ) -> None:
+#         return None
+#
--- fabjax/targets/cox_utils.py
+++ fabjax/targets/cox_utils.py
@@ -1,139 +1,154 @@
+# Taken from https://github.com/google-deepmind/annealed_flow_transport/blob/master/annealed_flow_transport/cox_process_utils.py
+
+"""Code for cox process density utilities."""
+
 import itertools
 
 import chex
-import numpy as np
-import pandas
-import jax.numpy as jnp
 import jax
+import jax.numpy as jnp
+import jax.scipy.linalg as slinalg
+import numpy as np
+
+# TypeDefs
+NpArray = np.ndarray
+Array = chex.Array
 
 
-def read_points(file_path):
-    df = pandas.read_csv(file_path)
-    x_pos, y_pos = np.array(df["data_x"]), np.array(df["data_y"])
-    pos = np.vstack([x_pos, y_pos]).T  # (B, 2)
-    return pos
+def get_bin_counts(array_in: NpArray,
+                   num_bins_per_dim: int) -> NpArray:
+  """Divide two dimensional input space into a grid and count points in each.
+
+  Point on the upper edge, which does happen in the data, go into the lower bin.
+  The occurrence of these points is an artefact of the rescaling done on data.
+
+  Args:
+    array_in: (num_points,2) containing points in square [0,1]^2
+    num_bins_per_dim: the number of bins per dimension for the grid.
+
+  Returns:
+    Numpy array of shape containing (num_bins_per_dim, num_bins_per_dim) counts.
+  """
+  chex.assert_rank(array_in, 2)
+  scaled_array = array_in * num_bins_per_dim
+  counts = np.zeros((num_bins_per_dim, num_bins_per_dim))
+  for elem in scaled_array:
+    flt_row, col_row = np.floor(elem)
+    row = int(flt_row)
+    col = int(col_row)
+    # Deal with the case where the point lies exactly on upper/rightmost edge.
+    if row == num_bins_per_dim:
+      row -= 1
+    if col == num_bins_per_dim:
+      col -= 1
+    counts[row, col] += 1
+  return counts
 
 
-def get_bin_counts(array_in, num_bins_per_dim):
-    scaled_array = array_in * num_bins_per_dim
-    counts = np.zeros((num_bins_per_dim, num_bins_per_dim))
-    for elem in scaled_array:
-        flt_row, col_row = np.floor(elem)
-        row = int(flt_row)
-        col = int(col_row)
-        # Deal with the case where the point lies exactly on upper/rightmost edge.
-        if row == num_bins_per_dim:
-            row -= 1
-        if col == num_bins_per_dim:
-            col -= 1
-        counts[row, col] += 1
-    return counts
+def get_bin_vals(num_bins: int) -> NpArray:
+  grid_indices = jnp.arange(num_bins)
+  bin_vals = jnp.array([
+      jnp.array(elem) for elem in itertools.product(grid_indices, grid_indices)
+  ])
+  return bin_vals
 
 
-def get_bin_vals(num_bins: int):
-    grid_indices = np.arange(num_bins)
-    bin_vals = np.array(
-        [np.array(elem) for elem in itertools.product(grid_indices, grid_indices)]
-    )
-
-    return bin_vals
+def gram(kernel, xs: Array) -> Array:
+  """Given a kernel function and an array of points compute a gram matrix."""
+  return jax.vmap(lambda x: jax.vmap(lambda y: kernel(x, y))(xs))(xs)
 
 
-def th_batch_kernel_fn(x, y, signal_variance, num_grid_per_dim, raw_length_scale):
-    x = x.reshape(-1, 1, x.shape[-1])  # Bx1xL
-    y = y.reshape(1, -1, x.shape[-1])  # 1xBxL
-    dist = jnp.linalg.norm(x - y, axis=2) / (num_grid_per_dim * raw_length_scale)
-    return signal_variance * jnp.exp(-dist)  # BxB
+def kernel_func(x: Array,
+                y: Array,
+                signal_variance: Array,
+                num_grid_per_dim: int,
+                raw_length_scale: Array) -> Array:
+  """Compute covariance/kernel function.
+
+  K(m,n) = signal_variance * exp(-|m-n|/(num_grid_per_dim*raw_length_scale))
+
+  Args:
+    x: First point shape (num_spatial_dim,)
+    y: Second point shape (num_spatial_dim,)
+    signal_variance: non-negative scalar.
+    num_grid_per_dim: Number of grid points per spatial dimension.
+    raw_length_scale: Length scale of the undiscretized process.
+
+  Returns:
+    Scalar value of covariance function.
+  """
+  chex.assert_equal_shape([x, y])
+  chex.assert_rank(x, 1)
+  normalized_distance = jnp.linalg.norm(x - y, 2) / (
+      num_grid_per_dim * raw_length_scale)
+  return signal_variance * jnp.exp(-normalized_distance)
 
 
-def get_latents_from_white(white, const_mean, cholesky_gram):
-    """
-    white: (B,D)
-    const_mean: scalar
-    cholesky_gram: (D,D)
-    """
-    return jnp.einsum("ij,bj->bi", cholesky_gram, white) + const_mean
+def poisson_process_log_likelihood(latent_function: Array,
+                                   bin_area: Array,
+                                   flat_bin_counts: Array) -> Array:
+  """Discretized Poisson process log likelihood.
+
+  Args:
+    latent_function: Intensity per unit area of shape (total_dimensions,)
+    bin_area: Scalar bin_area.
+    flat_bin_counts: Non negative integer counts of shape (total_dimensions,)
+
+  Returns:
+    Total log likelihood of points.
+  """
+  chex.assert_rank([latent_function, bin_area], [1, 0])
+  chex.assert_equal_shape([latent_function, flat_bin_counts])
+  first_term = latent_function * flat_bin_counts
+  second_term = -bin_area * jnp.exp(latent_function)
+  return jnp.sum(first_term+second_term)
 
 
-def get_white_from_latents(latents, const_mean, cholesky_gram):
-    """
-    latents: (B,D)
-    const_mean: scalar
-    cholesky_gram: (D,D)
-    """
-    solve_traingular = lambda b: jax.scipy.linalg.solve_triangular(cholesky_gram, b, lower=True)
-    white = jax.vmap(solve_traingular)(latents - const_mean)
-    return white
+def get_latents_from_white(white: Array, const_mean: Array,
+                           cholesky_gram: Array) -> Array:
+  """Get latents from whitened representation.
+
+  Let f = L e + mu where e is distributed as standard multivariate normal.
+  Then Cov[f] = LL^T .
+  In the present case L is assumed to be lower triangular and is given by
+  the input cholesky_gram.
+  mu_zero is a constant so that mu_i = const_mean for all i.
+
+  Args:
+    white: shape (total_dimensions,) e.g. (900,) for a 30x30 grid.
+    const_mean: scalar.
+    cholesky_gram: shape (total_dimensions, total_dimensions)
+
+  Returns:
+    points in the whitened space of shape (total_dimensions,)
+  """
+  chex.assert_rank([white, const_mean, cholesky_gram], [1, 0, 2])
+  latent_function = jnp.matmul(cholesky_gram, white) + const_mean
+  chex.assert_equal_shape([latent_function, white])
+  return latent_function
 
 
-def poisson_process_log_likelihood(latent_function, bin_area, flat_bin_counts: chex.Array):
-    """
-    latent_function: (B,D) "xi"
-    bin_area: Scalar  "\alpha"
-    flat_bin_counts: (D)  "yi"
-    """
-    first_term = latent_function * flat_bin_counts[None]  # (B,D)
-    second_term = -bin_area * jnp.exp(latent_function)
-    return jnp.sum(first_term + second_term, axis=1)  # (B,)
+def get_white_from_latents(latents: Array,
+                           const_mean: Array,
+                           cholesky_gram: Array) -> Array:
+  """Get whitened representation from function representation.
 
+  Let f = L e + mu where e is distributed as standard multivariate normal.
+  Then Cov[f] = LL^T and e = L^-1(f-mu).
+  In the present case L is assumed to be lower triangular and is given by
+  the input cholesky_gram.
+  mu_zero is a constant so that mu_i = const_mean for all i.
 
-class Cox:
-    def __init__(self, file_name, num_bins_per_dim, use_whitened):
-        self.use_whitened = use_whitened
-        self._num_latents = num_bins_per_dim ** 2
-        # self._num_grid_per_dim = num_bins_per_dim
+  Args:
+    latents: shape (total_dimensions,) e.g. (900,) for a 30x30 grid.
+    const_mean: scalar.
+    cholesky_gram: shape (total_dimensions, total_dimensions)
 
-        bin_counts = jnp.array(get_bin_counts(read_points(file_name), num_bins_per_dim))
-        self._flat_bin_counts = bin_counts.flatten()
-
-        self._poisson_a = 1.0 / self._num_latents
-        self._signal_variance = 1.91
-        self._beta = 1.0 / 33
-
-        # torch
-        self._bin_vals = jnp.array(get_bin_vals(num_bins_per_dim))
-        short_kernel_func = lambda x, y: th_batch_kernel_fn(
-            x, y, self._signal_variance, num_bins_per_dim, self._beta
-        )
-        self._gram_matrix = short_kernel_func(self._bin_vals, self._bin_vals)
-        self._cholesky_gram = jnp.linalg.cholesky(self._gram_matrix)
-        self._white_gaussian_log_normalizer = (
-            -0.5 * self._num_latents * np.log(2.0 * np.pi)
-        )  # float
-
-        half_log_det_gram = jnp.sum(jnp.log(jnp.abs(jnp.diag(self._cholesky_gram))))
-        self._unwhitened_gaussian_log_normalizer = (
-            -0.5 * self._num_latents * np.log(2.0 * np.pi) - half_log_det_gram
-        )  # tensor scalar
-
-        self._mu_zero = np.log(126.0) - 0.5 * self._signal_variance  # tensor scalar
-
-        if use_whitened:
-            self.evaluate_log_density = self.whitened_posterior_log_density
-        else:
-            self.evaluate_log_density = self.unwhitened_posterior_log_density
-
-    def whitened_posterior_log_density(self, white):
-        # B, _ = white.shape
-        quadratic_term = -0.5 * jnp.sum(white ** 2, axis=1)  # (B,)
-        prior_log_density = self._white_gaussian_log_normalizer + quadratic_term  # (B,)
-        latent_function = get_latents_from_white(
-            white, self._mu_zero, self._cholesky_gram
-        )  # (B,D)
-        log_likelihood = poisson_process_log_likelihood(
-            latent_function, self._poisson_a, self._flat_bin_counts
-        )  # (B,)
-        return prior_log_density + log_likelihood  # (B,)
-
-    def unwhitened_posterior_log_density(self, latents: chex.Array):
-        white = get_white_from_latents(
-            latents, self._mu_zero, self._cholesky_gram
-        )  # (B,D)
-        prior_log_density = (
-            -0.5 * jnp.sum(white * white, axis=1)
-            + self._unwhitened_gaussian_log_normalizer
-        )  # (B,)
-        log_likelihood = poisson_process_log_likelihood(
-            latents, self._poisson_a, self._flat_bin_counts
-        )  # (B,)
-        return prior_log_density + log_likelihood
+  Returns:
+    points in the whitened space of shape (total_dimensions,)
+  """
+  chex.assert_rank([latents, const_mean, cholesky_gram], [1, 0, 2])
+  white = slinalg.solve_triangular(
+      cholesky_gram, latents - const_mean, lower=True)
+  chex.assert_equal_shape([latents, white])
+  return white
